88709d05cffefc29ed38c6098eea2e62353ece4d
estump2021 <estump21@nscds.org>
2020-02-10 15:12:25 +0000

    sounds

--- Whack-A-Mole/finalproject.js
+++ Whack-A-Mole/finalproject.js
@@ -1,32 +1,45 @@
 
-var score = 0 
-var randMole = Math.floor((Math.random() * 8) + 1  );
+var score = 0;
+var randMole;// = Math.floor((Math.random() * 8) + 1  );
+var time = 30;
+var boinkSound = new Audio("Boink2.mp3");
 
-$('.mole').click(
-    function(){
+$('.mole').click(function(){
         $(this).slideUp();
         score++;
         $("#score").text(score);
-        
+        boinkSound.play(); 
     }
-    
 );
+
 $('#start').click(function() {
         $('.mole').slideUp();
-    }
+        var moleShowing = setInterval(function showMole() {
+            var randMole = Math.floor((Math.random() * 8) + 1  );
+            $(".mole"+randMole).slideDown();
+            setTimeout(function hideMole() {
+                $(".mole"+randMole).slideUp();
+                },1500)
+            console.log(randMole);
+        }, 500),;
+        var timerStopping = setInterval(function timeCoundown(){
+            time--;
+            $("#time").text(time); 
+        }, 1000)
+        if (time <= 0) {
+            function timerstop() {
+                clearInterval(moleShowing);
+                clearInterval(timerStopping);
+            } 
+        }
+    },
+     
 );
 
 
-setTimeout(function showMole() {
-    //pick a number between 1 and 8
-    var randMole = Math.floor((Math.random() * 8) + 1  );
-    $(".mole"+randMole).slideDown();
 
-    //setTimeout(alertFunc, 3000);
-}, 3000) 
  
 
 
 
 
-console.log(randMole);
