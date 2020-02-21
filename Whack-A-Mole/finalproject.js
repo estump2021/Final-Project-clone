377b2ed042caef8beb31e97bdfdbcb777b02266e
estump2021 <estump21@nscds.org>
2020-02-21 15:30:59 +0000

    titlebar rework

--- Whack-A-Mole/finalproject.js
+++ Whack-A-Mole/finalproject.js
@@ -3,6 +3,7 @@ var score = 0;
 var randMole;// = Math.floor((Math.random() * 8) + 1  );
 var time = 30;
 var boinkSound = new Audio("Boink3.mp3")
+var level = 1;
 
 $('.mole').click(function(){
         $(this).slideUp();
@@ -12,7 +13,8 @@ $('.mole').click(function(){
     }
 );
 
-$('#start').click(function() {
+if (level == 1) {
+    $('#start').click(function() {
         $('.mole').slideUp();
        var moleStop = setInterval(function showMole() {
             if (time == 0) {
@@ -32,9 +34,14 @@ $('#start').click(function() {
             time--;
             $("#time").text(time); 
         }, 1000)
-    },
-     
-);
+        if (time == 0) {
+            if (score > 14) {
+            level++;
+            $("#level").text(level);
+            }
+        }
+    },);
+}
 
 
 
