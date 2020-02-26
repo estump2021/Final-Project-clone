eaa35f568a19269f2ab546f7432e46b64c067bd6
estump2021 <estump21@nscds.org>
2020-02-26 19:16:18 +0000

    level work

--- Whack-A-Mole/finalproject.js
+++ Whack-A-Mole/finalproject.js
@@ -3,7 +3,7 @@ var score = 0;
 var randMole;// = Math.floor((Math.random() * 8) + 1  );
 var time = 30;
 var boinkSound = new Audio("Boink3.mp3")
-var level = 1;
+var level = 2;
 
 $('.mole').click(function(){
         $(this).slideUp();
@@ -16,7 +16,7 @@ $('.mole').click(function(){
 if (level == 1) {
     $('#start').click(function() {
         $('.mole').slideUp();
-       var moleStop = setInterval(function showMole() {
+        var moleShowing = setInterval(function showMole() {
             if (time == 0) {
                 clearInterval(moleShowing);
             }
@@ -34,19 +34,23 @@ if (level == 1) {
             time--;
             $("#time").text(time); 
         }, 1000)
-        if (time == 0) {
-            if (score > 14) {
-            level++;
-            $("#level").text(level);
+        setTimeout(function levelUp() {
+            if (score >= 25) {
+                level++;
+                $("#level").text(level); 
+            } 
+            else {
+                alert("you Lose! press command + r to reset");
             }
-        }
+        },30000)
     },);
 }
+if (level == 2) {
+    
+}
+if (level == 3) {
 
-
-
- 
-
+}
 
 
 
